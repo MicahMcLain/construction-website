c7aa1094c4a88bb19fb0bab8ca6426fac45d7b93
MicahMcLain <mclainmicah1@gmail.com>
2024-02-22 21:03:24 -0500

    lighten colors on process page. still feels aggressive though. add BBB review button

--- app/components/ReverseProcessBubble.tsx
+++ app/components/ReverseProcessBubble.tsx
@@ -14,9 +14,9 @@ const ReverseProcessBubble = ({
         {description}
       </div>
       {/* Tail */}
-      <div className="w-2/3 top-1/2 transform -translate-y-1/2 h-0 border-t-4 border-l-4 border-amber-600"></div>
+      <div className="w-2/3 top-1/2 transform -translate-y-1/2 h-0 border-t-4 border-l-4 border-amber-300"></div>
       {/* Circle */}
-      <div className="w-20 h-20 bg-amber-600 rounded-full flex justify-center items-center text-white font-bold text-lg">
+      <div className="w-20 h-20 bg-amber-300 rounded-full flex justify-center items-center text-white font-bold text-lg">
         {title}
       </div>
     </div>
--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -33,6 +33,17 @@ const Sidebar = () => {
         </Flex>
       </Container>
       <a
+        href="https://www.bbb.org/cincinnati/customer-reviews/general-contractor/tri-b-contract-services-llc-in-moores-hill-in-90045568/add/"
+        target="_blank"
+        rel="nofollow"
+        className="self-center pb-4 pr-4"
+      >
+        <img
+          src="https://seal-cincinnati.bbb.org/customer-reviews/badge-7-bbb-90045568.png"
+          alt="Tri B Contract Services, LLC BBB Business Review"
+        />
+      </a>
+      <a
         href={schedulingWebsite}
         target={"_blank"}
         className="text-zinc-600 hover:text-black transition-colors self-center pb-4"
