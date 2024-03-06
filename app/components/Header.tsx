0056e6f9d28a28bcc7a0fa5de9c2ffd7e18cb498
MicahMcLain <mclainmicah1@gmail.com>
2024-03-06 16:02:50 -0500

    added styling for mobile

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,26 +1,105 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { FaFacebook } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 import { FaPhoneAlt } from "react-icons/fa";
+import { FiMenu, FiX } from "react-icons/fi";
+import { Container, Flex } from "@radix-ui/themes";
+import Link from "next/link";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const facebook =
     "https://www.facebook.com/profile.php?id=61556115710244&mibextid=sCpJLy";
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <div className="flex justify-evenly border-b border-black bg-gray-900 text-white py-4">
-      <div className="flex items-center">
-        <IoMdMail className="w-10 h-10" />
-        <p>: tribcontractservices@gmail.com</p>
+    <div className="relative flex flex-col md:flex-row justify-between items-center border-b border-black bg-gray-900 text-white py-4">
+      <div className="flex items-center mb-2 md:mb-0">
+        <IoMdMail className="w-10 h-10 pl-2" />
+        <p className="ml-2">: tribcontractservices@gmail.com</p>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-center mb-2 md:mb-0">
         <FaPhoneAlt className="w-8 h-8" />
-        <p>: 513-882-9110</p>
+        <p className="ml-2">: 513-882-9110</p>
       </div>
       <div>
-        <a href={facebook} target={"_blank"} className="self-center mb-2">
-          <FaFacebook color="blue" className="w-10 h-10 flex justify-end" />
+        <a href={facebook} target="_blank" className="self-center">
+          <FaFacebook color="blue" className="w-10 h-10 pr-2" />
         </a>
       </div>
+      {/* Hamburger Menu */}
+      <div className="lg:hidden flex top-0 right-0">
+        <button
+          onClick={toggleMenu}
+          className="text-white focus:outline-none focus:bg-gray-700 p-2"
+        >
+          {isMenuOpen ? (
+            <FiX className="w-6 h-6" />
+          ) : (
+            <FiMenu className="w-6 h-6" />
+          )}
+        </button>
+      </div>
+      {/* Mobile Menu */}
+      {isMenuOpen && (
+        <div className="fixed inset-0 bg-gray-900 text-white z-50 flex justify-center items-center">
+          <Container className="h-full pt-12">
+            <Flex className="flex-col justify-center h-full space-y-4">
+              <Link
+                href="/"
+                passHref
+                className="text-white hover:text-gray-300 border-t border-b border-white py-2 text-lg text-center"
+                onClick={toggleMenu}
+              >
+                Home
+              </Link>
+              <Link
+                href="/about"
+                passHref
+                className="text-white hover:text-gray-300 border-t border-b border-white py-2 text-lg text-center"
+                onClick={toggleMenu}
+              >
+                About Us
+              </Link>
+              <Link
+                href="/process"
+                passHref
+                className="text-white hover:text-gray-300 border-t border-b border-white py-2 text-lg text-center"
+                onClick={toggleMenu}
+              >
+                Our Process
+              </Link>
+              <Link
+                href="/services"
+                passHref
+                className="text-white hover:text-gray-300 border-t border-b border-white py-2 text-lg text-center"
+                onClick={toggleMenu}
+              >
+                Services
+              </Link>
+              <Link
+                href="/reviews"
+                passHref
+                className="text-white hover:text-gray-300 border-t border-b border-white py-2 text-lg text-center"
+                onClick={toggleMenu}
+              >
+                Reviews
+              </Link>
+              <button
+                onClick={toggleMenu}
+                className="text-white absolute top-0 right-0 m-4"
+              >
+                <FiX className="w-6 h-6" />
+              </button>
+            </Flex>
+          </Container>
+        </div>
+      )}
     </div>
   );
 };
--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,77 +1,76 @@
 "use client";
 import { Container, Flex } from "@radix-ui/themes";
-import classnames from "classnames";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import logo from "../logo_1.png";
 import YelpReview from "./YelpReview";
 import BBBReview from "./BBBReview";
 
 const Sidebar = () => {
-  const currentPath = usePathname();
-  const links = [
-    { href: "/", label: "Home" },
-    { href: "/about", label: "About Us" },
-    { href: "/process", label: "Our Process" },
-    { href: "/services", label: "Services" },
-    { href: "/reviews", label: "Reviews" },
-  ];
   const schedulingWebsite = "https://calendly.com/tribcontractservices";
 
   return (
-    <nav className="sidebar">
-      <Image
-        src={logo}
-        width={300}
-        height={300}
-        alt="logo"
-        className="max-h-[300px] pb-2"
-      ></Image>
-      <Container className="pb-4">
-        <Flex className="flex-col space-y-4 gap">
-          <Link href="/"></Link>
-          <NavLinks />
-        </Flex>
-      </Container>
-      <YelpReview />
-      <BBBReview />
-      <a
-        href={schedulingWebsite}
-        target={"_blank"}
-        className="text-zinc-600 hover:text-black transition-colors self-center pb-4 mb-4"
-      >
-        Schedule an Appointment
-      </a>
-    </nav>
-  );
-};
-
-const NavLinks = () => {
-  const currentPath = usePathname();
-  const links = [
-    { href: "/", label: "Home" },
-    { href: "/about", label: "About Us" },
-    { href: "/process", label: "Our Process" },
-    { href: "/services", label: "Services" },
-    { href: "/reviews", label: "Reviews" },
-  ];
-  return (
-    <ul className="">
-      {links.map((link) => (
-        <li className="pb-3" key={link.href}>
-          <Link
-            className={classnames({
-              "nav-link": true,
-              "!text-black": currentPath === link.href,
-            })}
-            href={link.href}
-          >
-            {link.label}
+    <nav className=" hidden md:block lg:flex flex-col fixed left-0 top-0 h-full w-[208px] border-r-2 border-black bg-stone-200 text-black">
+      <div className="flex items-center justify-between">
+        <div className="flex items-center">
+          <Image
+            src={logo}
+            width={208}
+            height={208}
+            alt="logo"
+            className="max-h-auto pb-2"
+          />
+        </div>
+      </div>
+      <Container className="py-4 pl-4">
+        <Flex className="flex-col space-y-4">
+          <Link href="/" passHref className="text-zinc-500 hover:text-black">
+            Home
           </Link>
-        </li>
-      ))}
-    </ul>
+          <Link
+            href="/about"
+            passHref
+            className="text-zinc-500 hover:text-black"
+          >
+            About Us
+          </Link>
+          <Link
+            href="/process"
+            passHref
+            className="text-zinc-500 hover:text-black"
+          >
+            Our Process
+          </Link>
+          <Link
+            href="/services"
+            passHref
+            className="text-zinc-500 hover:text-black"
+          >
+            Services
+          </Link>
+          <Link
+            href="/reviews"
+            passHref
+            className="text-zinc-500 hover:text-black"
+          >
+            Reviews
+          </Link>
+        </Flex>
+        <div className="flex justify-center py-8">
+          <YelpReview />
+          <BBBReview />
+        </div>
+
+        <a
+          href={schedulingWebsite}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-zinc-600 hover:text-white transition-colors self-center pb-4 mb-4"
+        >
+          Schedule an Appointment
+        </a>
+      </Container>
+    </nav>
   );
 };
 
