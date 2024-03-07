a8c73f99c0f07b986f382a0da778144ca3cca656
MicahMcLain <mclainmicah1@gmail.com>
2024-03-07 14:22:11 -0500

    removed cors to try to find the issue with deployment

--- next.config.js
+++ next.config.js
@@ -10,22 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-Requested-With, Authorization, Accept, Content-Type, Set-Cookie",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
