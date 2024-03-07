8a999cdeacc70b56b50894d7cd2e60fe6c5a340a
MicahMcLain <mclainmicah1@gmail.com>
2024-03-07 14:18:18 -0500

    added nextconfig back in

--- next.config.js
+++ next.config.js
@@ -1,5 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "seal-cincinnati.bbb.org",
+        port: "443",
+        pathname: "/customer-reviews/badge-7-bbb-90045568.png",
+      },
+    ],
+  },
   async headers() {
     return [
       {
