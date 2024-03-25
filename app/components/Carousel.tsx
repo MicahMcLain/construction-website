f6198073d7e09f69da3a1b1fb47bd0cfe763b949
MicahMcLain <mclainmicah1@gmail.com>
2024-03-25 12:50:16 -0400

    changed carousel to use ul/li instead of div

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -3,32 +3,27 @@ import { Carousel } from "react-responsive-carousel";
 import { CldImage } from "next-cloudinary";
 
 const ServiceCardCarousel: React.FC<{ urls: string[] }> = ({ urls }) => {
-  // const image = "Roger's Website/Basements/jo22h8cq4zhfgtnjhh0e";
   return (
-    <div className="flex">
-      <Carousel
-        showThumbs={false} // Optional: Set to true to display thumbnails
-        infiniteLoop // Enable continuous image rotation
-        autoPlay // Optional: Set to true for automatic image transition
-      >
-        {urls.map((url, index) => (
-          <CldImage
-            key={index}
-            src={url} //cloudinary public url
-            alt="Service image"
-            width="200"
-            height="200"
-          />
-        ))}
-      </Carousel>
-      {/* <div>
-        <CldImage
-          src={image} //cloudinary public url
-          alt="Service image"
-          width="200"
-          height="200"
-        />
-      </div> */}
+    <div>
+      <ul className="flex">
+        <Carousel
+          showThumbs={false} // Optional: Set to true to display thumbnails
+          infiniteLoop // Enable continuous image rotation
+          autoPlay // Optional: Set to true for automatic image transition
+        >
+          {urls.map((url, index) => (
+            <li key={index}>
+              {/* Add a container element */}
+              <CldImage
+                src={url}
+                alt="Service image"
+                width="200"
+                height="200"
+              />
+            </li>
+          ))}
+        </Carousel>
+      </ul>
     </div>
   );
 };
