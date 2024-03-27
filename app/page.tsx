595d12624558d64ffc56c462b30e982e0e9f5952
MicahMcLain <mclainmicah1@gmail.com>
2024-03-27 13:07:24 -0400

    added and style bg image for home screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import { Metadata } from "next";
 import Link from "next/link";
+import backgroundImage from "./background.png";
 
 const Hero = () => {
   return (
-    <section className="bg-gray-300 py-16 text-center">
+    <section className="bg-gray-300 py-16 text-center opacity-75">
       <h2 className="text-4xl font-bold mb-4">Building Dreams Together</h2>
       <p className="text-lg">Your trusted partner in construction projects</p>
     </section>
@@ -18,18 +19,18 @@ const Services = () => {
           <Link href="/services">Our Services</Link>
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-          <div className="bg-white p-6 shadow-lg">
+          <div className="bg-white p-6 shadow-lg opacity-75">
             <h3 className="text-xl font-bold mb-4">Construction Management</h3>
             <p>From planning to completion, we handle it all.</p>
           </div>
-          <div className="bg-white p-6 shadow-lg">
+          <div className="bg-white p-6 shadow-lg opacity-75">
             <h3 className="text-xl font-bold mb-4">Design & Build</h3>
             <p>
               Turn your vision into reality with our design and construction
               expertise.
             </p>
           </div>
-          <div className="bg-white p-6 shadow-lg">
+          <div className="bg-white p-6 shadow-lg opacity-75">
             <h3 className="text-xl font-bold mb-4">Renovation & Restoration</h3>
             <p>
               Breathe new life into old spaces with our renovation services.
@@ -43,7 +44,7 @@ const Services = () => {
 
 const About = () => {
   return (
-    <section id="about" className="bg-gray-300 py-16 text-center">
+    <section id="about" className="bg-gray-300 opacity-75 py-16 text-center">
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-8">
           <Link href="/about">About Us</Link>
@@ -60,7 +61,7 @@ const schedulingWebsite = "https://calendly.com/tribcontractservices";
 const Contact = () => {
   return (
     <section id="contact" className="py-16 text-center">
-      <div className="container mx-auto">
+      <div className="bg-white p-6 shadow-lg mx-auto w-1/3 opacity-75">
         <h2 className="text-3xl font-bold mb-8">Contact Us</h2>
         <p>Let&apos;s discuss your project.</p>
         <p>Phone: 513-882-9110</p>
@@ -79,7 +80,14 @@ const Contact = () => {
 
 const HomePage = () => {
   return (
-    <div>
+    <div
+      className="text-center py-16"
+      style={{
+        backgroundImage: `url(${backgroundImage.src})`,
+        backgroundSize: "cover", // Adjust background size as needed
+        backgroundPosition: "center", // Adjust position as needed
+      }}
+    >
       <Hero />
       <Services />
       <About />
